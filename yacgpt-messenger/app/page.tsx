d45b498ad0d5f2c4fed0c16df9bb6bcc410be266
Yacov Kopel <kokokopel94@gmail.com>
2023-04-11 12:37:25 -0700

    set up for page.tsx

---
+++ yacgpt-messenger/app/page.tsx
@@ -0,0 +1,11 @@
+function Homepage() {
+    return(
+        <div>
+            <h1>
+                YacGpt HERE to service you!
+            </h1>
+        </div>
+    );
+    
+}
+export default Homepage;
