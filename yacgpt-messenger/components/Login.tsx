e4750f7e04f49d480362757139f6454ac0d65f83
Yacov Kopel <kokokopel94@gmail.com>
2023-04-17 16:30:48 -0700

    configure OAuth with google cloud

--- yacgpt-messenger/components/Login.tsx
+++ yacgpt-messenger/components/Login.tsx
@@ -1,6 +1,12 @@
+'use client'
+import { signIn } from "next-auth/react";
+
 function Login () {
     return(
-        <div>Login</div>
+        <div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center text-center">
+        <img src="/images/gptlogo.png" alt="logo" />
+        <button onClick={()=> signIn("google")}
+        className="text-white font-bold text-3xl animate-pulse">Sign in to YacGpt</button></div>
     )
 }
 export default Login;
