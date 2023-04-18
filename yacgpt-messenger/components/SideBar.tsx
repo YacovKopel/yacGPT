07a39ed917ae8e8ac2fabedc2c7b659b82ac281d
Yacov Kopel <kokokopel94@gmail.com>
2023-04-18 13:16:16 -0700

    Chat input bar half way done

--- yacgpt-messenger/components/SideBar.tsx
+++ yacgpt-messenger/components/SideBar.tsx
@@ -9,7 +9,7 @@ import ChatRow from "./ChatRow";
 function SideBar() {
   const { data: session } = useSession();
   const [chats, loading, error]=useCollection(session && query(
-    collection(db, "users", session.user?.email!, "chats"),
+    collection(db, "users", session?.user?.email!, "chats"),
     orderBy("createdAt", "asc")
   ))
   return (
