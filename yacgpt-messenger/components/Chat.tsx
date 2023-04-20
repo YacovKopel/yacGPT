8f2292efc3cd48cc2a4818b0622a34d9a298b142
Yacov Kopel <kokokopel94@gmail.com>
2023-04-19 23:47:36 -0700

    added icon and prompt instructions for new chat

--- yacgpt-messenger/components/Chat.tsx
+++ yacgpt-messenger/components/Chat.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { db } from "@/firebase";
+import { ArrowDownCircleIcon } from "@heroicons/react/24/outline";
 import { collection, orderBy, query } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useCollection } from "react-firebase-hooks/firestore";
@@ -22,7 +23,16 @@ const [messages] =useCollection(session && query(
 
 
     return(
-        <div className="flex-1">
+        <div className="flex-1 overflow-y-auto overflow-x-hidden">
+            {messages?.empty &&(
+                <>
+                <p className="mt-10 text-white text-center">
+Type a prompt below to get started!
+                </p>
+                <ArrowDownCircleIcon className='h-10 w-10 mx-auto mt-5 text-white animate-bounce'/>
+                </>
+
+            )}
             {messages?.docs.map((message)=> (
             <Message key ={message.id} message={message.data()} />
             ))}
