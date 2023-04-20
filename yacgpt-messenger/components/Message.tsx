afacd632f76939baaadf8a8e437043b4afabef2d
Yacov Kopel <kokokopel94@gmail.com>
2023-04-19 23:40:40 -0700

    displaying ChatGPT response on screen

---
+++ yacgpt-messenger/components/Message.tsx
@@ -0,0 +1,22 @@
+import { DocumentData } from "firebase/firestore"
+
+
+type Props={
+    message: DocumentData
+}
+
+function Message ({message}: Props){
+    const isYacGPT=message.user.name==="YacGPT"
+    
+    return(
+<div className={`py-5 text-white ${isYacGPT && 'bg-[#434654]'}`}>
+    <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
+        <img src={message.user.avatar} alt="user picture" className="h-8 w-8" />
+        <p className="pt-1 text-sm">{message.text}</p>
+
+    </div>
+</div>
+    )
+}
+
+export default Message;
